a3865521419a10975453a5579190e8269a436748
Francesco Santi <88621180+FrancescoSanti1@users.noreply.github.com>
2023-07-18 22:13:46 +0200

    handle css for every button variations

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Button from './components/Button'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
+  const handleClick = () => {
+    console.log("ciao")
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Button variation={"primary"}>Button text</Button>
+      <Button onClick={handleClick} variation={"secondary"}>Button text</Button>
+      <Button variation={"success"}>Button text</Button>
+      <Button variation={"warning"} outline>Button text</Button>
+      <Button variation={"danger"} rounded outline>Button text</Button>
+      <Button>Button text 2</Button>
     </>
   )
 }
-
-export default App
