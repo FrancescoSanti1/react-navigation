42e589807279bb4989c2982e83c10e049d319896
Francesco Santi <88621180+FrancescoSanti1@users.noreply.github.com>
2023-07-22 11:04:50 +0200

    add Panel component for consistent styling and to avoid repeated Tailwind classes

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import Panel from "./Panel";
 
 export default function Dropdown({ options, value, onChange }) {
 
@@ -18,12 +19,12 @@ export default function Dropdown({ options, value, onChange }) {
     })
 
     return <div className="w-48 relative">
-        <div onClick={handleClick} className="hover:cursor-pointer flex justify-between items-center border rounded p-3 shoadow bg-white w-full">
+        <Panel onClick={handleClick} className="hover:cursor-pointer flex justify-between items-center">
             {value?.label || "Select..."}
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
             </svg>
-        </div>
-        {open && <div className="absolute top-full border rounded p-3 shadow bg-white w-full">{renderList}</div>}
+        </Panel>
+        {open && <Panel className="absolute top-full">{renderList}</Panel>}
     </div>
 }
