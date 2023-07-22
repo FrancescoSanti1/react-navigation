5205e58bd5ea2998505ed219245bd8956813e4e8
Francesco Santi <88621180+FrancescoSanti1@users.noreply.github.com>
2023-07-22 07:52:35 +0200

    add styling

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -4,22 +4,22 @@ import { useState } from "react";
 export default function Accordion({ items }) {
     const [expanded, setExpanded] = useState(0);
 
-    return <div>
+    return <div className="border-x border-t rounded">
         {items.map((item, i) => {
 
             const isExpanded = expanded === i;
             const icon = <span>{isExpanded
-                ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                ? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
                 </svg>
-                : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
                 </svg>
             }</span>
 
             return <div key={item.id}>
-                <p onClick={() => setExpanded(i)} className="hover:cursor-pointer flex">{item.label} {icon}</p>
-                {isExpanded && <p>{item.content}</p>}
+                <p onClick={() => setExpanded(i)} className="hover:cursor-pointer flex p-3 bg-gray-50 border-b justify-between items-center">{item.label} {icon}</p>
+                {isExpanded && <p className="border-b p-5">{item.content}</p>}
             </div >
         })}
     </div>;
