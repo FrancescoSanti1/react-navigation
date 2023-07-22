f5d24662b502b74039aed55c08c02b5abd06503c
Francesco Santi <88621180+FrancescoSanti1@users.noreply.github.com>
2023-07-22 12:16:06 +0200

    close dropdown on click outside the component

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,9 +1,25 @@
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import Panel from "./Panel";
 
 export default function Dropdown({ options, value, onChange }) {
 
     const [open, setOpen] = useState(false);
+
+    const divEl = useRef();
+
+    useEffect(() => {
+        const handler = (event) => {
+            if (!divEl.current) return;
+
+            if (!divEl.current.contains(event.target)) {
+                setOpen(false);
+            }
+        }
+
+        document.addEventListener("click", handler, true);
+
+        return () => { document.removeEventListener("click", handler, true) }
+    }, []);
 
     const handleClick = () => {
         setOpen(!open);
@@ -18,7 +34,7 @@ export default function Dropdown({ options, value, onChange }) {
         return <div onClick={() => handleSelected(option)} key={option.value} className="hover:cursor-pointer hover:bg-sky-100 rounded p-1">{option.label}</div>
     })
 
-    return <div className="w-48 relative">
+    return <div ref={divEl} className="w-48 relative">
         <Panel onClick={handleClick} className="hover:cursor-pointer flex justify-between items-center">
             {value?.label || "Select..."}
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
