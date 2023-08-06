fe49dc61f5d926b274550df8f3572637548d1abe
Francesco Santi <88621180+FrancescoSanti1@users.noreply.github.com>
2023-08-06 18:36:10 +0200

    fix modal opening

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -13,8 +13,8 @@ export default function Modal({ onClose, children, actions }) {
 
     return createPortal(
         <div>
-            <div onClick={onClose} className="absolute inset-0 bg-gray-300 opacity-80"></div>
-            <div className="absolute inset-40 p-10 bg-white">
+            <div onClick={onClose} className="fixed inset-0 bg-gray-300 opacity-80"></div>
+            <div className="fixed inset-40 p-10 bg-white">
                 <div className="flex flex-col justify-between h-full">
                     {children}
                     <div className="action-bar flex justify-end">{actions}</div>
