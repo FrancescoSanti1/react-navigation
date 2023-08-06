5465543ede9eaab16f3d55d27616aa790c22ce74
Francesco Santi <88621180+FrancescoSanti1@users.noreply.github.com>
2023-08-06 18:30:28 +0200

    add useEffect to avoid scrolling when the modal is open

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,11 +1,24 @@
+import { useEffect } from "react";
 import { createPortal } from "react-dom"
 
-export default function Modal({ onClose }) {
+export default function Modal({ onClose, children, actions }) {
+
+    useEffect(() => {
+        document.body.classList.add("overflow-hidden");
+
+        return () => {
+            document.body.classList.remove("overflow-hidden");
+        }
+    }, []);
+
     return createPortal(
         <div>
             <div onClick={onClose} className="absolute inset-0 bg-gray-300 opacity-80"></div>
             <div className="absolute inset-40 p-10 bg-white">
-                Modal content.
+                <div className="flex flex-col justify-between h-full">
+                    {children}
+                    <div className="action-bar flex justify-end">{actions}</div>
+                </div>
             </div>
         </div>,
         document.querySelector(".modal-container"));
