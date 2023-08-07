8c73289cb9296f9ada54ea06793f2c08b32d2050
Francesco Santi <88621180+FrancescoSanti1@users.noreply.github.com>
2023-08-07 16:27:38 +0200

    add SortableTable component, config sortable columns

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,8 +1,13 @@
+import { Fragment } from "react"
+
 export default function Table({ data, config, keyFn }) {
     return <table className="table-auto border-spacing-2">
         <thead>
             <tr className="border-b-2">
                 {config.map(th => {
+                    if (th.header) {
+                        return <Fragment key={th.label}>{th.header()}</Fragment>;
+                    }
                     return <th key={th.label}>{th.label}</th>
                 })}
             </tr>
--- src/pages/TablePage.jsx
+++ src/pages/TablePage.jsx
@@ -1,4 +1,4 @@
-import Table from "../components/Table";
+import SortableTable from "../components/SortableTable";
 
 export default function TablePage() {
 
@@ -12,7 +12,8 @@ export default function TablePage() {
     const config = [
         {
             label: "Fruit",
-            render: (fruit) => fruit.name
+            render: (fruit) => fruit.name,
+            sortValue: (fruit) => fruit.name
         },
         {
             label: "Color",
@@ -20,11 +21,12 @@ export default function TablePage() {
         },
         {
             label: "Score",
-            render: (fruit) => fruit.score
+            render: (fruit) => fruit.score,
+            sortValue: (fruit) => fruit.score
         }
     ];
 
     const keyFn = (fruit) => fruit.name;
 
-    return <Table data={data} config={config} keyFn={keyFn} />
+    return <SortableTable data={data} config={config} keyFn={keyFn} />
 }
