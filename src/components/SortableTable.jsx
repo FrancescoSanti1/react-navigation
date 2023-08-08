6988611e0220606ca8a6df72a7d29c4d41897ba8
Francesco Santi <88621180+FrancescoSanti1@users.noreply.github.com>
2023-08-08 10:20:49 +0200

    add header icons

--- src/components/SortableTable.jsx
+++ src/components/SortableTable.jsx
@@ -20,6 +20,29 @@ export default function SortableTable(props) {
         }
     }
 
+    const icons = (label, sortBy, sortOrder) => {
+        if (label !== sortBy) {
+            return <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M3 7.5L7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5" />
+            </svg>;
+        }
+
+        if (!sortOrder) {
+            return <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M3 7.5L7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5" />
+            </svg>;
+        } else if (sortOrder === "asc") {
+            return <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 6.75L12 3m0 0l3.75 3.75M12 3v18" />
+            </svg>;
+        } else if (sortOrder === "desc") {
+            return <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 17.25L12 21m0 0l-3.75-3.75M12 21V3" />
+            </svg>;
+        }
+
+    };
+
     const updatedConfig = config.map(col => {
         if (!col.sortValue) {
             return col;
@@ -27,7 +50,14 @@ export default function SortableTable(props) {
 
         return {
             ...col,
-            header: () => <th onClick={() => handleClick(col.label)}>{col.label} (sortable)</th>
+            header: () => (
+                <th onClick={() => handleClick(col.label)} className="cursor-pointer hover:bg-gray-100">
+                    <div className="flex items-center">
+                        {col.label}
+                        {icons(col.label, sortBy, sortOrder)}
+                    </div>
+                </th>
+            )
         }
     });
 
